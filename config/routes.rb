0cfdde286e6561a36a67203592dc382a7aae2682
Onishi1129 <taku.hot.2545@gmail.com>
2021-09-11 17:24:34 +0000

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
+    
   end
 
 end
